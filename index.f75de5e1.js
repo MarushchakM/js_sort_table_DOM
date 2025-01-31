b4c94a134a45e577f18b3768f7622369e16c7544
marushchak <marushchak099@gmail.com>
2025-01-31 19:53:14 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,19 @@
+"use strict";
+const table = document.querySelector("table");
+const headers = table.querySelectorAll("th");
+const tbody = table.querySelector("tbody");
+headers.forEach((header, index)=>{
+    header.addEventListener("click", ()=>{
+        const rows = Array.from(tbody.querySelectorAll("tr"));
+        rows.sort((rowA, rowB)=>{
+            const cellA = rowA.cells[index].textContent.trim();
+            const cellB = rowB.cells[index].textContent.trim();
+            if (index === 2) return Number(cellA) - Number(cellB);
+            else if (index === 3) return Number(cellA.replace(/[$,]/g, "")) - Number(cellB.replace(/[$,]/g, ""));
+            else return cellA.localeCompare(cellB);
+        });
+        tbody.append(...rows);
+    });
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
